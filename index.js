fe42809360b97b430e2b41237e6e07f1e4b8bb6c
ArturGr <graefenstein@petesso.de>
2018-02-23 14:11:08 +0200

    Update index.js
    added animation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import React, {Component} from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, Animated } from 'react-native'
 
 function getTexts(arr) {
     let words = arr.split(' ')
@@ -8,15 +8,23 @@ function getTexts(arr) {
             i.split('')
         )
         return words
-    }else{
+    } else {
         return [words]
     }
 }
 
 export default class extends Component {
+    constructor(props) {
+        super(props);
+        let fontSize = props.style && StyleSheet.flatten(props.style).fontSize;
+        this.state = {
+            fontSize: new Animated.Value(fontSize),
+        };
+    }
+
     render() {
-        const {style, letterSpacing, textAlign, containerStyle, children} = this.props
-        let {wordSpacing} = this.props
+        const { style, letterSpacing, textAlign, containerStyle, children } = this.props
+        let { wordSpacing } = this.props
         if (!wordSpacing) {
             wordSpacing = letterSpacing * 3
         }
@@ -29,19 +37,19 @@ export default class extends Component {
         return (
             <View style={containerStyle}>
                 <View
-                    style={[styles.wrapper, {marginLeft: -1 * wordSpacing}, textAlign === 'center' && {justifyContent: 'center'}, textAlign === 'right' && {justifyContent: 'flex-end'}]}>
+                    style={[styles.wrapper, { marginLeft: -1 * wordSpacing }, textAlign === 'center' && { justifyContent: 'center' }, textAlign === 'right' && { justifyContent: 'flex-end' }]}>
                     {
                         words.map((i, ind) =>
                             <View
                                 style={[
-                                    styles.word, {marginLeft: wordSpacing},
-                                    textAlign === 'center' && {justifyContent: 'center'}
+                                    styles.word, { marginLeft: wordSpacing },
+                                    textAlign === 'center' && { justifyContent: 'center' }
                                 ]}
                                 key={ind}>
                                 {
-                                    i.map((j, index) => <Text
-                                        style={[style, index > 0 && {marginLeft: letterSpacing}]}
-                                        key={index}>{j}</Text>)
+                                    i.map((j, index) => <Animated.Text
+                                        style={[style, index > 0 && { marginLeft: letterSpacing }, this.props.animated && { fontSize: this.state.fontSize }]}
+                                        key={index}>{j}</Animated.Text>)
                                 }
                             </View>
                         )
@@ -49,6 +57,14 @@ export default class extends Component {
                 </View>
             </View>
         )
+    }
+
+    setFontSizeTo(size, time) {
+        Animated.timing(this.state.fontSize, // The value to drive
+            {
+                toValue: size,
+                duration: time,
+            }).start(); // Start the animation
     }
 }
 
@@ -66,5 +82,3 @@ const styles = StyleSheet.create({
     }
 
 })
-
-
